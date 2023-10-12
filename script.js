83fc81f289038c1567e4f09f343b6c741af79fa5
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-10-12 10:31:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
-  // your code here
+  // Remove leading and trailing spaces
+  text = text.trim();
+
+  // Check if the string is empty or consists only of spaces
+  if (text === '') {
+    return '';
+  }
+
+  // Return the first character of the cleaned string
+  return text[0];
 }
 
 // Do not change the code below
